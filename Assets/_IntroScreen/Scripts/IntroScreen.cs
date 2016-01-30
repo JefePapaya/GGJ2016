013c94082e42dd530e585901ee2679e5592b053c
Caio Tyghorn Victor <caiovictormota@gmail.com>
2016-01-30 20:21:48 -0200

    Version with Menu and Intro. Probably will be necessary to resize images.

---
+++ Assets/_IntroScreen/Scripts/IntroScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroScreen : MonoBehaviour {
+
+    public float timer = 2.0f;
+
+    void Update(){
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            Application.LoadLevel("MainMenu");
+        }
+    }
+}
