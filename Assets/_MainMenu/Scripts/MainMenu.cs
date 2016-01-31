3d28cc66ac128b825715f227a4c4e4890e5d6d5d
Caio Tyghorn Victor <caiovictormota@gmail.com>
2016-01-31 00:43:45 -0200

    Alterado botão do Main Menu para responder ao espaço

--- Assets/_MainMenu/Scripts/MainMenu.cs
+++ Assets/_MainMenu/Scripts/MainMenu.cs
@@ -2,10 +2,17 @@
 using System.Collections;
 
 public class MainMenu : MonoBehaviour {
-
+/*
     public void ChangeScene(string sceneName){
 
         Application.LoadLevel("Level_01");
+    }*/
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space")){
+            Application.LoadLevel("Level_01");
+        }
     }
 
 }
